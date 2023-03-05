8396187a7d94febe220ede1cd7313377fe519f0b
Sam Callaway <calls002@gmail.com>
2023-03-05 22:29:26 +0000

    Written function to create markdown from input

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const questions = require("./questions");
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log('Success!'));
 }
 
 // function to initialize program
 function init() {
-    console.log("Welcome to the README generator")
+    console.log("Welcome to the README generator. Press enter to skip any sections")
     inquirer.prompt(questions).then((data) => {
-
+        writeToFile("README.md",generateMarkdown(data))
     })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let output = `# ${data.Title}`
 
-`;
-}
+  if (data.Description === ""){}else{output = output.concat(`\n\n# Description\n\n${data.Description}`)}
+  let contents = `\n\n## Table of Contents`
+  for (property in data){
+    if(property === "Email" || property === "GitHub" || property === "Title" || property === "Description"){}else{
+      if(data[property] === ""){}else{
+      contents = contents.concat(`\n\n[${property}](#${property.replace(" ","-").toLowerCase()})`)}
+    }
+  }
+
+  if(data.GitHub === "" && data.Email === ""){}
+  else{contents = contents.concat(`\n\n[Questions](#questions)`)}
+
+  output = output.concat(contents)
+
+  for (property in data){
+    if(property === "Email" || property === "GitHub" || property === "Title" || property === "Description"){}else{
+      if(data[property] === ""){}
+      else
+      {
+        output = output.concat(`\n\n## ${property}\n\n${data[property]}`)
+      }}}
+
+  if(data.GitHub === "" && data.Email === ""){}
+  else{
+    output = output.concat(`\n\n## Questions
+    \nIf you have any questions, please contact me at the below:`)
+    if(data.GitHub === ""){}else{output = output.concat(`\nhttps://github.com/${data.GitHub}`)}
+    if(data.Email === ""){}else{output = output.concat(`\n${data.Email}`)}
+  }
+  
+  return output;}
+
 
 module.exports = generateMarkdown;
